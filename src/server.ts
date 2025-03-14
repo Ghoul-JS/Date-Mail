132acb1ad6151459a0e0c1d911535494869b2cda
Ghoul-JS <jmanuelgut170@gmail.com>
2025-03-13 23:22:34 -0500

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+// require('dotenv/config');
+import app from './app'
+// import dbConnect from './database';
+const PORT = 3001;
+
+// dbConnect()
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`)
+})
