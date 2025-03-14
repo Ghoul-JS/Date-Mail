0768960f42a0b3001c4fafa4b827c5a0b2869759
Ghoul-JS <jmanuelgut170@gmail.com>
2025-03-14 12:18:16 -0500

    server and db setted

---
+++ src/database.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+import mongoose from "mongoose"
+
+
+const DB = process.env.DB_URI || ""
+
+// const DB_DEP = process.env.DB_DEPLOY
+
+const db = () => {
+    const connect = () => {
+        mongoose.connect(DB, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true
+          })
+            .then(() => {
+              console.log('Connection to DB is success');
+            })
+            .catch(error => {
+              console.error('Connection fail', error);
+            });
+    }
+    connect();
+}
+
+export default db;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config'
 // require('dotenv/config');
 import app from './app'
-// import dbConnect from './database';
+import dbConnect from './database';
 const PORT = 3001;
 
-// dbConnect()
+dbConnect()
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`)
