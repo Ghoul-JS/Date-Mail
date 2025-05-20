5919bd006f612de187433efda578adf4692495b0
Ghoul-JS <jmanuelgut170@gmail.com>
2025-05-20 14:41:47 -0500

    fix: cors url to production

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
   
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://date-mail.onrender.com',
     credentials: true,
   }));
 
