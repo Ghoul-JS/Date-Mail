ef7d58aae423d9d5c584739c22c0f25439e02195
Ghoul-JS <jmanuelgut170@gmail.com>
2025-05-20 15:27:29 -0500

    fix: url cors origin to local (switched)

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
   
 app.use(cors({
-    origin: 'https://date-mail.onrender.com',
+    origin: ['http://localhost:3000'],
     credentials: true,
   }));
 
