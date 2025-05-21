c0176b8391f80e64b66ff138880a415208def888
Ghoul-JS <jmanuelgut170@gmail.com>
2025-05-21 13:50:50 -0500

    fix: token get way

--- src/controllers/dataAuth.controller.ts
+++ src/controllers/dataAuth.controller.ts
@@ -4,16 +4,25 @@ import { Request, Response, NextFunction } from 'express';
 
 export const dataAuth = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token){ res.status(401).json({ message: 'Token requerido' }); return}
+    // 🔄 Acepta el token desde cookie o desde header
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      res.status(401).json({ message: 'Token requerido' });
+      return;
+    }
 
     const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
     const user = await User.findById(decoded.id).select('name email image');
 
-    if (!user) {res.status(404).json({ message: 'Usuario no encontrado' }); return } 
+    if (!user) {
+      res.status(404).json({ message: 'Usuario no encontrado' });
+      return;
+    }
 
     res.status(200).json(user);
   } catch (err) {
     res.status(401).json({ message: 'Token inválido' });
   }
 };
+
